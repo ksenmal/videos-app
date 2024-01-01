5aee3035a7e4a206b4e85ad485e3ad3bd51bfc91
ksenmal <ks.malysheva@gmail.com>
2024-01-01 17:35:34 +0100

    init commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ModalService } from '../services/modal.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent {
+
+  constructor(private modalService: ModalService) { }
+
+  openAuthModal(event: Event) {
+    event.preventDefault();
+    this.modalService.toggleModal();
+  }
+
+}
